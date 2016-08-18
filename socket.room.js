08c521be752795223a6fc848333efce86fa11bdd
newbaid <newbaid@naver.com>
2016-08-18 17:18:30 +0900

    채팅1

---
+++ socket.room.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+
+var server = require('http').createServer();
+
+server.listen(8000, function(){
+	console.log('Server Running at http://localhost:8000');
+});
+
+server.on('request', function(req, res){
+	/*test*/console.log('request Call');
+
+	fs.readFile( 'catting.html', function(error, data){
+		res.writeHead( 200, { 'Content-Type' : 'text/html' } );
+		res.end( data );
+	});
+});
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on( 'connection', function(socket){
+	socket.on( 'join', function(data){
+		console.log(data);
+		socket.join(data);
+		socket.room = data;
+	});
+
+	socket.on( 'message', function(data){
+
+		console.log( 'id : %s, msg : %s, date : %s', data.id, data.message, data.date );
+		io.sockets.in( socket.room ).emit('message', data);
+	});
+});
